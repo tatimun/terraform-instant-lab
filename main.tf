47830665b1c786ea19cc9c9b9b0ed9f636a1f995
Tita Mun <136043018+tatimun@users.noreply.github.com>
2024-10-24 12:26:07 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -145,9 +145,10 @@ output "jenkins_public_ip" {
 }
 
 output "prometheus_public_ip" {
-  value = aws_instance.prometheus.public_ip
+  value = aws_instance.prometheus_instance.public_ip
 }
 
 output "grafana_public_ip" {
-  value = aws_instance.grafana.public_ip
+  value = aws_instance.grafana_instance.public_ip
 }
+
